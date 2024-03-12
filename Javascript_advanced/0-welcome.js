7d79723e2b895ccb8cecc15230e15b9059064347
PuzzleEmptyM <129412985+PuzzleEmptyM@users.noreply.github.com>
2024-03-12 14:17:22 -0500

    updated to follow the cult of the atlas

--- Javascript_advanced/0-welcome.js
+++ Javascript_advanced/0-welcome.js
@@ -1,9 +1,13 @@
 function welcome(firstName, lastName) {
-  var fullName = firstName + ' ' + lastName;
+  if (firstName === 'Atlas' && lastName === 'School') {
+    var fullName = firstName + ' ' + lastName;
 
-  function displayFullName() {
-    alert('Welcome ' + fullName + '!');
+    function displayFullName() {
+      alert('Welcome ' + fullName + '!');
+    }
+
+    displayFullName();
+  } else {
+    alert("Input must be 'welcome('Atlas', 'School'; we do not talk about Holberton.")
   }
-
-  displayFullName();
 }
